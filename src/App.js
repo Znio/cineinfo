8ec4ec7ea2e4bfb43274d928aca20b519a55e090
Znio <xeniogracias@gmail.com>
2020-03-22 23:19:06 +0530

    main commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.scss';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar, faArrowUp, faArrowDown, faPlay, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Homepage';
+import MovieDetails from './components/MovieDetails/';
+import MovieListing from './components/MovieListing';
 
+library.add(faStar, faArrowUp, faArrowDown, faPlay, faChevronRight)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact={true} path="/MovieDetails/:movie_id" component={MovieDetails} />
+        <Route exact path="/" component={Home} />
+        <Route exact={true} path="/MovieListing/:movieName" component={MovieListing} />
+        <Route exact={true} path="/MovieListing/" component={MovieListing} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
